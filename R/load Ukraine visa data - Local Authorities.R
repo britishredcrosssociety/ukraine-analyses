d6840e51dffdfed71cfbbdebf1dcb9928dea414c
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-11-18 11:56:38 +0100

    Update regex

--- R/load Ukraine visa data - Local Authorities.R
+++ R/load Ukraine visa data - Local Authorities.R
@@ -21,24 +21,23 @@ out <- content(req, "text")
 doc <- read_html(out)
 
 # Get URLs of each visa data file by scraping the href of any <a> tags within anything that has the class "attachment-inline"
-visa_data_urls <- 
-  doc |> 
-  html_nodes(xpath="//*[contains(@class, 'attachment-inline')]//a/@href") |>
+visa_data_urls <-
+  doc |>
+  html_nodes(xpath = "//*[contains(@class, 'attachment-inline')]//a/@href") |>
   html_text()
 
 # Download each dataset if it doesn't already exist
 for (url in visa_data_urls) {
   visa_filename <- str_extract(url, "Ukraine.*")
   visa_filepath <- file.path(visa_path, visa_filename)
-  
+
   if (!file.exists(visa_filepath)) {
     GET(
-      url, 
+      url,
       write_disk(visa_filepath)
     )
     print(paste0("Downloaded ", visa_filename))
-    Sys.sleep(0.75)  # Scrape responsibly with a pause between requests
-    
+    Sys.sleep(0.75) # Scrape responsibly with a pause between requests
   } else {
     print(paste0(visa_filename, " has already been downloaded - skipping"))
   }
@@ -46,31 +45,30 @@ for (url in visa_data_urls) {
 
 # ---- Helper functions for wrangling visa data ----
 wrangle_visa_summary <- function(d, visa_date) {
-  d |> 
-    as_tibble(.name_repair = "unique") |> 
-    rename(Type = `...2`) |> 
-    fill(Location, .direction = "down") |> 
-    na.omit() |> 
-    mutate(Date = visa_date) |> 
+  d |>
+    as_tibble(.name_repair = "unique") |>
+    rename(Type = `...2`) |>
+    fill(Location, .direction = "down") |>
+    na.omit() |>
+    mutate(Date = visa_date) |>
     relocate(Date)
 }
 
 wrangle_visa_data <- function(d, visa_date, country = "^E") {
-  d |> 
-    as_tibble() |> 
-    rename_with(function(x) "ltla21_code", contains("ONS code")) |> 
-    filter(str_detect(ltla21_code, country)) |> 
-    mutate(Date = visa_date) |> 
-    relocate(Date) |> 
-    mutate(across(starts_with("Number"), as.character)) |> 
-    
+  d |>
+    as_tibble() |>
+    rename_with(function(x) "ltla21_code", contains("ONS code")) |>
+    filter(str_detect(ltla21_code, country)) |>
+    mutate(Date = visa_date) |>
+    relocate(Date) |>
+    mutate(across(starts_with("Number"), as.character)) |>
     # Standardise column names (some have footnotes; some say "Numbers" rather than "Number")
-    rename_with(function(x) "Number of visa applications", contains("applications")) |> 
+    rename_with(function(x) "Number of visa applications", contains("applications")) |>
     rename_with(function(x) "Number of arrivals", contains("arrivals"))
 }
 
 convert_number_columns <- function(d) {
-  d |> 
+  d |>
     mutate(across(starts_with("Number"), ~ as.integer(str_remove(.x, "<")) - 1))
 }
 
@@ -86,25 +84,23 @@ visas_scotland <- list()
 visas_ni <- list()
 
 # List counter
-i = 1
+i <- 1
 
 # Loop over each visa data file and load/clean the data
 for (visa_file in visa_files) {
   # Get date the data was published
-  visa_date <- 
-    visa_file |> 
-    str_remove(".ods") |> 
-    str_remove("^[A-Za-z\\_\\-]+") |> 
-    str_remove("th|rd") |> 
+  visa_date <-
+    visa_file |>
+    str_extract("(\\d{1,2}|\\d\\w+)_\\w+_\\d{4}") |>
     dmy()
-  
+
   # The number of rows to skip when running read_ods() differs depending on the date/format of the file
   summary_skip <- case_when(
     visa_date <= ymd("2022-05-01") ~ 6,
     visa_date == ymd("2022-05-03") ~ 7,
     TRUE ~ 8
   )
-  
+
   england_skip <-
     case_when(
       visa_date <= ymd("2022-05-01") ~ 6,
@@ -112,40 +108,40 @@ for (visa_file in visa_files) {
       visa_date == ymd("2022-06-21") ~ 7,
       TRUE ~ 8
     )
-  
+
   wales_scotland_skip <- case_when(
     visa_date <= ymd("2022-05-01") ~ 8,
     visa_date == ymd("2022-05-03") ~ 9,
     TRUE ~ 10
   )
-  
+
   ni_skip <-
     case_when(
       visa_date <= ymd("2022-05-01") ~ 6,
       TRUE ~ 8
     )
-  
+
   # UK summary
   tmp_summary <- read_ods(file.path(visa_path, visa_file), sheet = "Summary", skip = summary_skip)
   visas_summary[[i]] <- wrangle_visa_summary(tmp_summary, visa_date)
-  
+
   # England
   tmp_england <- read_ods(file.path(visa_path, visa_file), sheet = "LTLA_-_England", skip = england_skip)
   if (ncol(tmp_england) > 5) tmp_england <- tmp_england |> select(1:5)
   visas_england[[i]] <- wrangle_visa_data(tmp_england, visa_date, country = "^E")
-  
+
   # Wales
   tmp_wales <- read_ods(file.path(visa_path, visa_file), sheet = "Wales", skip = wales_scotland_skip)
   visas_wales[[i]] <- wrangle_visa_data(tmp_wales, visa_date, country = "^W")
-  
+
   # Scotland
   tmp_scotland <- read_ods(file.path(visa_path, visa_file), sheet = "Scotland", skip = wales_scotland_skip)
   visas_scotland[[i]] <- wrangle_visa_data(tmp_scotland, visa_date, country = "^S")
-  
+
   # Northern Ireland
   tmp_ni <- read_ods(file.path(visa_path, visa_file), sheet = "Northern_Ireland", skip = ni_skip)
   visas_ni[[i]] <- wrangle_visa_data(tmp_ni, visa_date, country = "^N")
-  
+
   i <- i + 1
   print(paste0("Imported data from ", visa_date))
 }
@@ -157,13 +153,13 @@ visas_ltla21_wales <- bind_rows(visas_wales)
 visas_ltla21_scotland <- bind_rows(visas_scotland)
 visas_ltla21_ni <- bind_rows(visas_ni)
 
-visas_ltla21_uk <- 
+visas_ltla21_uk <-
   bind_rows(
-    visas_ltla21_england |> rename(ltla21_name = `Lower tier local authority name`), 
-    visas_ltla21_wales |> rename(ltla21_name = `Upper tier local authority name`), 
-    visas_ltla21_scotland |> rename(ltla21_name = `Upper tier local authority name`), 
+    visas_ltla21_england |> rename(ltla21_name = `Lower tier local authority name`),
+    visas_ltla21_wales |> rename(ltla21_name = `Upper tier local authority name`),
+    visas_ltla21_scotland |> rename(ltla21_name = `Upper tier local authority name`),
     visas_ltla21_ni |> rename(ltla21_name = `Local authority name`)
-  ) |> 
+  ) |>
   mutate(Country = case_when(
     str_detect(ltla21_code, "^E") ~ "England",
     str_detect(ltla21_code, "^W") ~ "Wales",
@@ -181,9 +177,9 @@ visas_ltla21_ni <- convert_number_columns(visas_ltla21_ni)
 # Remove everything except the wrangled visa data
 # rm(list = ls()[!str_detect(ls(), "visas_ltla21")])
 rm(
-  visa_path, visa_url, ua, req, out, doc, visa_data_urls, url, visa_filename, visa_filepath, 
-  wrangle_visa_summary, wrangle_visa_data, visa_files, i, visa_file, visa_date, 
-  summary_skip, england_skip, wales_scotland_skip, ni_skip, 
-  tmp_summary, tmp_england, tmp_wales, tmp_scotland, tmp_ni, 
+  visa_path, visa_url, ua, req, out, doc, visa_data_urls, url, visa_filename, visa_filepath,
+  wrangle_visa_summary, wrangle_visa_data, visa_files, i, visa_file, visa_date,
+  summary_skip, england_skip, wales_scotland_skip, ni_skip,
+  tmp_summary, tmp_england, tmp_wales, tmp_scotland, tmp_ni,
   visas_summary, visas_england, visas_wales, visas_scotland, visas_ni
 )
